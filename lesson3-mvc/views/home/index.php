75df438cdfe39da080bb5074291af982969758a1
thienth32 <thienth32@gmail.com>
2020-02-13 07:54:01 +0700

    update

--- lesson3-mvc/views/home/index.php
+++ lesson3-mvc/views/home/index.php
@@ -4,15 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style>
-        h2{
-            color: green;
-        }
-        p{
-            max-width: 400px;
-        }
-    </style>
+    <title>Homepage</title>
+   
 </head>
 <body>
     
