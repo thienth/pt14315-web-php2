24ba10bf3cb7511f9cd6e421ae405c7ae6d89179
thienth32 <thienth32@gmail.com>
2020-02-24 15:58:01 +0700

    update layout

---
+++ lesson3-mvc/views/layouts/_admin_share/script.blade.php
@@ -0,0 +1,23 @@
+<!-- jQuery -->
+<script src="{{ADMIN_ASSET_URL}}plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="{{ADMIN_ASSET_URL}}plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="{{ADMIN_ASSET_URL}}plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="{{ADMIN_ASSET_URL}}dist/js/adminlte.js"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{ADMIN_ASSET_URL}}dist/js/demo.js"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="{{ADMIN_ASSET_URL}}plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="{{ADMIN_ASSET_URL}}plugins/raphael/raphael.min.js"></script>
+<script src="{{ADMIN_ASSET_URL}}plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="{{ADMIN_ASSET_URL}}plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="{{ADMIN_ASSET_URL}}plugins/chart.js/Chart.min.js"></script>
+
+<!-- PAGE SCRIPTS -->
+<script src="{{ADMIN_ASSET_URL}}dist/js/pages/dashboard2.js"></script>
---
+++ lesson3-mvc/views/layouts/_admin_share/style.blade.php
@@ -0,0 +1,8 @@
+<!-- Font Awesome Icons -->
+<link rel="stylesheet" href="{{ADMIN_ASSET_URL}}plugins/fontawesome-free/css/all.min.css">
+<!-- overlayScrollbars -->
+<link rel="stylesheet" href="{{ADMIN_ASSET_URL}}plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{ADMIN_ASSET_URL}}dist/css/adminlte.min.css">
+<!-- Google Font: Source Sans Pro -->
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
